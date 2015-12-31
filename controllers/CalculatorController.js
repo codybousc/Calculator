01b0b29027853cd99c5f0131a6d8fb3d9c74e939
Cody Bouscaren <codybousc@gmail.com>
2015-12-31 08:54:55 -0800

    Fixes controller issue

--- controllers/CalculatorController.js
+++ controllers/CalculatorController.js
@@ -1,10 +1,21 @@
-calculator = angular.module("calculator", []);
-
-calculator.controller('CalculatorController', ['$scope',
+angular.module("calculator", []).controller('CalculatorController', ['$scope',
 function($scope) {
+  $scope.result = 0;
+  $scope.one = 1;
+  $scope.two = 2;
+  $scope.test = "WHAT'S HAPPPENING?";
+
+$scope.assignVar = function() {
+  console.log("making it to assignVar function")
+  varOrderingArray = [];
+  varOrderingArray.push(variable);
+  console.log(varOrderingArray);
+}
+
+$scope.calculate = function(firstNum, secondNum) {
+
+}
 
 
 
-
-
-}]); 
+}]);
