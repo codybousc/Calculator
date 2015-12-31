0ca4e20b983e21a043336a14244f1605907824c3
Cody Bouscaren <codybousc@gmail.com>
2015-12-31 12:16:32 -0800

    Adds basic caculating functionality

--- controllers/CalculatorController.js
+++ controllers/CalculatorController.js
@@ -4,16 +4,48 @@ function($scope) {
   $scope.one = 1;
   $scope.two = 2;
   $scope.test = "WHAT'S HAPPPENING?";
-
-$scope.assignVar = function() {
-  console.log("making it to assignVar function")
-  varOrderingArray = [];
-  varOrderingArray.push(variable);
-  console.log(varOrderingArray);
+  $scope.varOrderingArray = [];
+  $scope.firstNum;
+  $scope.secondNum;
+  $scope.thirdNum;
+$scope.choseOperator = function(operator) {
+  $scope.chosenOperation = operator;
 }
 
-$scope.calculate = function(firstNum, secondNum) {
+$scope.assignVar = function(variable) {
+  //get chosen number to show up on screen
+  $scope.result = variable;
+  $scope.varOrderingArray.push(variable);
+  for(var i = 0; i < $scope.varOrderingArray.length; i++) {
+    $scope.firstNum = $scope.varOrderingArray[0];
+    $scope.secondNum = $scope.varOrderingArray[1];
+    $scope.thirdNum = $scope.varOrderingArray[2];
+  }
+}
 
+$scope.calculate = function() {
+  console.log($scope.firstNum, $scope.secondNum);
+  console.log($scope.chosenOperation);
+
+  switch ($scope.chosenOperation) {
+    case 'add':
+        $scope.result = $scope.firstNum + $scope.secondNum;
+        break;
+    case 'subtract':
+        $scope.result = $scope.firstNum - $scope.secondNum;
+        break;
+    case 'multiply':
+        $scope.result = $scope.firstNum * $scope.secondNum;
+        break;
+    case 'divide':
+        $scope.result = $scope.firstNum / $scope.secondNum;
+        break; 
+  }
+}
+
+$scope.clearOrderingArr = function() {
+  $scope.varOrderingArray = [];
+  $scope.result = 0;
 }
 
 
